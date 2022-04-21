6396d0771f6f06441112841b1b18b13830aedec7
konivan <konivan08@gmail.com>
2022-04-21 16:37:06 +0300

    some work

--- src/components/Content/CardItem/CardItem.tsx
+++ src/components/Content/CardItem/CardItem.tsx
@@ -22,11 +22,22 @@ const CardItem:FC<Props> = ({item}) => {
   return (
     <div className="item-wrap">
       <img src={item.imagePath} alt={item.name} />
-      <div className='text-wrap'>
+      <div className="text-wrap">
         <div className="item-name">{item.name}</div>
         <div className="item-price">{item.price}$</div>
-        <button onClick={addHandler} className="item-btn">В корзину</button>
-        <ItemCount count={count} setCount={setCount}/>
+        <button onClick={addHandler} className='item-btn'>
+          <a
+            href="#_"
+            className="px-3.5 py-1.5 mb-0.5 relative rounded group text-white font-semibold inline-block"
+          >
+            <span className="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
+            <span className="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
+            <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
+            <span className="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
+            <span className="relative">В корзину</span>
+          </a>
+        </button>
+        <ItemCount count={count} setCount={setCount} />
       </div>
     </div>
   );
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -27,7 +27,7 @@ const Content:FC = () => {
           <button onClick={() => sortPriceHandler(content)} className='relative bottom-3'>
             <a
               href="#_"
-              className="font-semibold relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+              className="font-semibold relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
               <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
               <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
               <span className="relative text-white">По убыванию</span>
@@ -36,7 +36,7 @@ const Content:FC = () => {
           <button onClick={() => sortLowerPriceHandler(content)} className='relative bottom-3 ml-4 '>
             <a
               href="#_"
-              className="font-semibold relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+              className="font-semibold relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
               <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
               <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
               <span className="relative text-white ">По возрастанию</span>
