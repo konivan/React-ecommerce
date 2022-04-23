6c78b10b77cffe4d2961f84d396901af61fc3bb0
konivan <konivan08@gmail.com>
2022-04-23 16:31:28 +0300

    search

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -24,6 +24,33 @@ const Header: FC = () => {
       <div className="header-logo-wrap">
         <img src={logoImg} alt="logo" />
       </div>
+      <div className="model-input-wrapper">
+        <input className="model-input" placeholder="Поиск по модели"></input>
+        <button className="input-btn">
+          <a
+            href="#_"
+            className="absolute bottom-7 bg-purple-600 font-bold ml-2 h-9 inline-flex items-center justify-center px-6 overflow-hidden text-indigo-600 transition duration-300 ease-out border-2 border-purple-800 rounded-full shadow-md group">
+            <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-600 group-hover:translate-x-0 ease">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+              </svg>
+            </span>
+            <span className="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">
+              Поиск
+            </span>
+            <span className="relative invisible">Button Text</span>
+          </a>
+        </button>
+      </div>
       <button
         onClick={() => {
           setIsCartShow(!isCartShow);
@@ -41,11 +68,11 @@ const Header: FC = () => {
               <img src={item.imagePath} alt={item.name} />
             </div>
             <div className="cart-count">{`${item.price}$ x ${item.count}`}</div>
-            <button
-              onClick={() => removeHandler(item._id)}>
+            <button onClick={() => removeHandler(item._id)}>
               <a
                 href="#_"
-                className="relative left-2 bottom-2.5 px-4 py-1 font-semibold text-white group">
+                className="relative left-2 bottom-2.5 px-4 py-1 font-semibold text-white group"
+              >
                 <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-purple-500 group-hover:bg-purple-700 group-hover:skew-x-12"></span>
                 <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-purple-700 group-hover:bg-purple-500 group-hover:-skew-x-12"></span>
                 <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
